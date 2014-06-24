f4341277104fd0e337dd53d5b51c325dbbe0a7bb
Jason Salerno <mrjsin@gmail.com>
2014-06-23 21:51:21 -0500

    Added first SAB class

---
+++ SimpleAB/SimpleAddressBook/SimpleAddressBook.h
@@ -0,0 +1,13 @@
+//
+//  SimpleAddressBook.h
+//  SimpleAB
+//
+//  Created by Jason Salerno on 6/23/14.
+//  Copyright (c) 2014 Jason Salerno. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SimpleAddressBook : NSObject
+
+@end
---
+++ SimpleAB/SimpleAddressBook/SimpleAddressBook.m
@@ -0,0 +1,13 @@
+//
+//  SimpleAddressBook.m
+//  SimpleAB
+//
+//  Created by Jason Salerno on 6/23/14.
+//  Copyright (c) 2014 Jason Salerno. All rights reserved.
+//
+
+#import "SimpleAddressBook.h"
+
+@implementation SimpleAddressBook
+
+@end
