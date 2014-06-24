de759f23dd31e282e677a5124e8ef946564e2036
Jason Salerno <mrjsin@gmail.com>
2014-06-23 21:31:17 -0500

    minor update in comments

--- SimpleAB/SABAppDelegate.m
+++ SimpleAB/SABAppDelegate.m
@@ -2,7 +2,7 @@
 //  SABAppDelegate.m
 //  SimpleAB
 //
-//  Created by Jason Salerno on 6/23/14.
+//  Created by Jason Salerno (MeltedSpork) on 6/23/14.
 //  Copyright (c) 2014 Jason Salerno. All rights reserved.
 //
 
--- SimpleAB/SABMasterViewController.m
+++ SimpleAB/SABMasterViewController.m
@@ -2,7 +2,7 @@
 //  SABMasterViewController.m
 //  SimpleAB
 //
-//  Created by Jason Salerno on 6/23/14.
+//  Created by Jason Salerno (MeltedSpork) on 6/23/14.
 //  Copyright (c) 2014 Jason Salerno. All rights reserved.
 //
 
